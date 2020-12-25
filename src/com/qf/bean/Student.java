62942cb0b4f519096484ba7bbb7238db8e9d4f0b
aya-yukari <1394683742@qq.com>
2020-12-25 11:27:21 +0800

    1231232

--- src/com/qf/bean/Student.java
+++ src/com/qf/bean/Student.java
@@ -12,4 +12,8 @@ public class Student {
     public static final double MATH_PI = 3.14159265;
     public String aya;
 
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
